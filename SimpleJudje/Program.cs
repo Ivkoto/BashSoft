89b4e5a0c4ef588ac6cc8c75d496bf0119f045b7
Ivaylo Kostov <ikostov87@gmail.com>
2018-10-16 01:24:24 +0300

    Created new solution for testing and compare files

---
+++ SimpleJudje/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SimpleJudje
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
---
+++ SimpleJudje/Tester.cs
@@ -0,0 +1,12 @@
+﻿using BashSoft;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleJudje
+{
+    class Tester
+    {
+        public static void CompareContent(string userOutputPath, string expectedOutputPath)        {            OutputWriter.WriteMessageOnNewLine("Reading files...");            string mismatchPath = GetMismatchPath(expectedOutputPath);        }        private static string GetMismatchPath(string expectedOutputPath)        {            int indexOf = expectedOutputPath.LastIndexOf('\\');            string directoryPath = expectedOutputPath.Substring(0, indexOf);            return directoryPath + @"\Mismatches.txt";        }
+    }
+}

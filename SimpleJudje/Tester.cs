39d9af417c3076b531436bf10236e29efe1c14c1
Ivaylo Kostov <ikostov87@gmail.com>
2018-10-16 09:46:32 +0300

    Fix Tester.cs file reading error.

--- SimpleJudje/Tester.cs
+++ SimpleJudje/Tester.cs
@@ -7,6 +7,19 @@ namespace SimpleJudje
 {
     class Tester
     {
-        public static void CompareContent(string userOutputPath, string expectedOutputPath)        {            OutputWriter.WriteMessageOnNewLine("Reading files...");            string mismatchPath = GetMismatchPath(expectedOutputPath);        }        private static string GetMismatchPath(string expectedOutputPath)        {            int indexOf = expectedOutputPath.LastIndexOf('\\');            string directoryPath = expectedOutputPath.Substring(0, indexOf);            return directoryPath + @"\Mismatches.txt";        }
+        public static void CompareContent(string userOutputPath, string expectedOutputPath)
+        {
+            OutputWriter.WriteMessageOnNewLine("Reading files...");
+
+            string mismatchPath = GetMismatchPath(expectedOutputPath);
+        }
+
+        private static string GetMismatchPath(string expectedOutputPath)
+        {
+            int indexOf = expectedOutputPath.LastIndexOf('\\');
+            string directoryPath = expectedOutputPath.Substring(0, indexOf);
+            return directoryPath + @"\Mismatches.txt";
+        }
+
     }
 }

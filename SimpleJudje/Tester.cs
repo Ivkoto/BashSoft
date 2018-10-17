f761c5e2364721bcb6f899998acd97abcd5b5f69
Ivaylo Kostov <ikostov87@gmail.com>
2018-10-18 02:07:16 +0300

    Added new methods in Tester class

--- SimpleJudje/Tester.cs
+++ SimpleJudje/Tester.cs
@@ -1,6 +1,7 @@
 ﻿using BashSoft;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace SimpleJudje
@@ -12,6 +13,15 @@ namespace SimpleJudje
             OutputWriter.WriteMessageOnNewLine("Reading files...");
 
             string mismatchPath = GetMismatchPath(expectedOutputPath);
+
+            string[] actualOutputLines = File.ReadAllLines(userOutputPath);
+            string[] expectedOutputLines = File.ReadAllLines(expectedOutputPath);
+
+            bool hasMismatches;
+            string[] mismatches = GetLineWithPossibleMismatche(actualOutputLines, expectedOutputLines, out hasMismatches);
+
+            PrintOutput(mismatches, hasMismatches, mismatchPath);
+            OutputWriter.WriteMessageOnNewLine("Files read!");
         }
 
         private static string GetMismatchPath(string expectedOutputPath)
@@ -21,5 +31,14 @@ namespace SimpleJudje
             return directoryPath + @"\Mismatches.txt";
         }
 
+        private static string[] GetLineWithPossibleMismatche(string[] actialOutputLines, string[] expectedOutputLines, out bool hasMismatches)
+        {
+
+        }
+
+        private static void PrintOutput(string[] mismatches, bool hasMismatch, string mismatchPath)
+        {
+
+        }
     }
 }

446e2af1f35576662eac01cf6d25cc24f109c1e5
Ivaylo Kostov <ikostov87@gmail.com>
2018-10-20 22:04:08 +0300

    Refactor and reassemble all project

---
+++ BashSoft/SimpleJudge/Tester.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace BashSoft
+{
+    public static class Tester
+    {
+        public static void CompareContent(string userOutputPath, string expectedOutputPath)
+        {
+            OutputWriter.WriteMessageOnNewLine("Reading files...");
+
+            string mismatchPath = GetMismatchPath(expectedOutputPath);
+
+            string[] actualOutputLines = File.ReadAllLines(userOutputPath);
+            string[] expectedOutputLines = File.ReadAllLines(expectedOutputPath);
+
+            bool hasMismatches;
+            string[] mismatches = GetLineWithPossibleMismatche(actualOutputLines, expectedOutputLines, out hasMismatches);
+
+            PrintOutput(mismatches, hasMismatches, mismatchPath);
+            OutputWriter.WriteMessageOnNewLine("Files read!");
+        }
+
+        private static string GetMismatchPath(string expectedOutputPath)
+        {
+            int indexOf = expectedOutputPath.LastIndexOf('\\');
+            string directoryPath = expectedOutputPath.Substring(0, indexOf);
+            return directoryPath + @"\Mismatches.txt";
+        }
+
+        private static string[] GetLineWithPossibleMismatche(string[] actualOutputLines, string[] expectedOutputLines, out bool hasMismatch)
+        {
+            hasMismatch = false;
+            string output = string.Empty;
+
+            string[] mismatches = new string[actualOutputLines.Length];
+            OutputWriter.WriteMessageOnNewLine("Comparng files...");
+
+            for (int index = 0; index < actualOutputLines.Length; index++)
+            {
+                string actualLine = actualOutputLines[index];
+                string expectedLine = expectedOutputLines[index];
+
+                if (!actualLine.Equals(expectedLine))
+                {
+                    output = string.Format($"Mismatch at line {0} -- expected: \"{1}\", actual: \"{2}\"", index, expectedLine, actualLine);
+                    output += Environment.NewLine;
+                    hasMismatch = true;
+                }
+                else
+                {
+                    output = actualLine;
+                    output += Environment.NewLine;
+                }
+
+                mismatches[index] = output;
+            }
+
+            return mismatches;
+        }
+
+        private static void PrintOutput(string[] mismatches, bool hasMismatch, string mismatchPath)
+        {
+            if (hasMismatch)
+            {
+                foreach (var line in mismatches)
+                {
+                    OutputWriter.WriteMessageOnNewLine(line);
+                }
+
+                File.WriteAllLines(mismatchPath, mismatches);
+            }
+            else
+            {
+                OutputWriter.WriteMessageOnNewLine("Files are identical.There are no mismatches.");
+            }
+        }
+    }
+}

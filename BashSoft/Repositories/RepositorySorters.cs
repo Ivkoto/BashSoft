3b68f49c80ea3a62fbe81ad942d4a2583c60a6bf
Ivaylo Kostov <ikostov87@gmail.com>
2018-11-21 01:58:43 +0200

    Finished with repository sorters class

--- BashSoft/Repositories/RepositorySorters.cs
+++ BashSoft/Repositories/RepositorySorters.cs
@@ -1,28 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace BashSoft.Repositories
 {
     public static class RepositorySorters
     {
-        public static void OrderAndTake(Dictionary<string, List<int>> studentsWithScores, string sorter, int studentsToTake)
+        public static void OrderAndTake(Dictionary<string, List<int>> wantedData, string comparision, int studentsToTake)
         {
-
+            comparision = comparision.ToLower();
+            if (comparision == "ascending")
+            {
+                OrderAndTake(wantedData, studentsToTake, CompareInOrder);
+            }
+            else if (comparision == "descending")
+            {
+                OrderAndTake(wantedData, studentsToTake, CompareDescendingOrder);
+            }
+            else
+            {
+                OutputWriter.DisplayExeptions(ExceptionMessages.InvalidComparisonQuery);
+            }
         }
 
-        private static void OrderAndTake(Dictionary<string, List<int>> studentsWithScores, int studentsToTake, 
+        private static void OrderAndTake(Dictionary<string, List<int>> wantedData, int studentsToTake,
             Func<KeyValuePair<string, List<int>>, KeyValuePair<string, List<int>>, int> comparisonFunc)
         {
 
         }
 
+        private static Dictionary<string, List<int>> GetSortedStudents (Dictionary<string, List<int>> wantedData, int studentsToTake,
+            Func<KeyValuePair<string, List<int>>, KeyValuePair<string, List<int>>, int> comparisonFunc)
+        {
+            int studentsTaken = 0;
+            var studentsSorted = new Dictionary<string, List<int>>();
+            var nextInOrder = new KeyValuePair<string, List<int>>();
+            bool isSorted = false;
+
+            while (studentsTaken < studentsToTake)
+            {
+                isSorted = true;
+
+                foreach (var studentScore in wantedData)
+                {
+                    if (!string.IsNullOrEmpty(nextInOrder.Key))
+                    {
+                        int comparisonResult = comparisonFunc(studentScore, nextInOrder);
+                        if (comparisonResult >= 0 && !studentsSorted.ContainsKey(studentScore.Key))
+                        {
+                            nextInOrder = studentScore;
+                            isSorted = false;
+                        }
+                    }
+                    else
+                    {
+                        if (!studentsSorted.ContainsKey(studentScore.Key))
+                        {
+                            nextInOrder = studentScore;
+                            isSorted = false;
+                        }
+                    }
+                }
+
+                if (!isSorted)
+                {
+                    studentsSorted.Add(nextInOrder.Key, nextInOrder.Value);
+                    studentsTaken++;
+                    nextInOrder = new KeyValuePair<string, List<int>>();
+                }                
+            }
+
+            return studentsSorted;
+        }
+
         private static int CompareInOrder(KeyValuePair<string, List<int>> firstValue, KeyValuePair<string, List<int>> secondValue)
         {
-            int totalCoresOfFirst = 0;
+            int totalScoresOfFirst = 0;
             foreach (int mark in firstValue.Value)
             {
-                totalCoresOfFirst += mark;
+                totalScoresOfFirst += mark;
             }
 
             int totalScoreSecond = 0;
@@ -31,7 +86,7 @@ namespace BashSoft.Repositories
                 totalScoreSecond += mark;
             }
 
-            return totalScoreSecond.CompareTo(totalCoresOfFirst);
+            return totalScoreSecond.CompareTo(totalScoresOfFirst);
         }
 
         private static int CompareDescendingOrder(KeyValuePair<string, List<int>> firstValue, KeyValuePair<string, List<int>> secondValue)
